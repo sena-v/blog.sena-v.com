d11cc7d33a1461e80a675cbe31d96e30343030ac
sena-v <sethna.u@gmail.com>
2020-12-28 23:39:48 +0900

    タイトル表示の高速化

--- src/components/header.js
+++ src/components/header.js
@@ -1,7 +1,7 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
-import back from '../images/titleback.png'
+import back from '../images/titleback.jpg'
 
 const Header = ({ siteTitle }) => (
   <header
